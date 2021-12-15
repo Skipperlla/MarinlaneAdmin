951fd283a13ace4abd6d56cb41b211333e3b3157
Ömer Esmer <68515357+Skipperlla@users.noreply.github.com>
2021-12-15 10:22:38 +0300

    Update SidebarDropDown.tsx

--- src/components/SidebarDropDown.tsx
+++ src/components/SidebarDropDown.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import Link from "next/link";
 const SidebarDropDown = () => {
+  const dropdown = [
+    { label: "Upcoming", target: "/booking/upcoming", icon: "calendar-check" },
+    { label: "Past", target: "/booking/past", icon: "calendar-minus" },
+    {
+      label: "Cancelled",
+      target: "/booking/cancelled",
+      icon: "calendar-times",
+    },
+  ];
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
     <>
       <div
         className="relative flex rounded-md cursor-pointer items-center mb-4 hover:bg-gray-200 transition-all mx-auto justify-between"
         style={{ padding: "8px 35px 8px 20px" }}
+        onClick={() => setIsOpen(!isOpen)}
       >
         <div className="text-base flex items-center justify-center">
           <span className="w-5 mr-2">
@@ -24,22 +36,30 @@ const SidebarDropDown = () => {
           />
         </div>
       </div>
-      <div className="pl-6 bg-transparent text-sm  ">
-        <div
-          style={{ padding: "8px 30px 8px 15px" }}
-          className="hover:bg-gray-200 rounded-md cursor-pointer transition-all"
-        >
-          <div className="text-base flex items-center justify-center">
-            <span className="w-5 mr-1">
-              <FontAwesomeIcon
-                icon="calendar-check"
-                className="text-primary-sideBarIconColor"
-              />
-            </span>
-            <span className="text-primary-sideBarIconColor">Bookings</span>
-          </div>
+      {isOpen && (
+        <div className="pl-6 bg-transparent text-sm">
+          {dropdown.map((item, index) => {
+            return (
+              <Link href={item.target} key={index}>
+                <a
+                  style={{ padding: "8px 30px 8px 15px" }}
+                  className="text-base flex items-center hover:bg-gray-200 rounded-md cursor-pointer transition-all"
+                >
+                  <span className="w-5 mr-1">
+                    <FontAwesomeIcon
+                      icon={item.icon as IconProp}
+                      className="text-primary-sideBarIconColor"
+                    />
+                  </span>
+                  <span className="text-primary-sideBarIconColor">
+                    {item.label}
+                  </span>
+                </a>
+              </Link>
+            );
+          })}
         </div>
-      </div>
+      )}
     </>
   );
 };
